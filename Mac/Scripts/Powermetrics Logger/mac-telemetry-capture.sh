71fb64f3ef483a705b4ceb93de50c6005fca4370
Federico Deis <43861713+fdeis@users.noreply.github.com>
2026-09-18 08:57:22 -0500

    Update mac-telemetry-capture.sh

--- Mac/Scripts/Powermetrics Logger/mac-telemetry-capture.sh
+++ Mac/Scripts/Powermetrics Logger/mac-telemetry-capture.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
 
-#########################################################################################################
-# IMPORTANT:  This software is supplied to you by Mobidelio in
-# consideration of your agreement to the following terms, and your use, installation,
-# modification or redistribution of this Mobidelio software constitutes acceptance of these
-# terms.  If you do not agree with these terms, please do not use or install this Mobidelio
-# software.
-#
-# This script is licensed to you as part of the MobileNow Management subscription. Mobidelio
-# grants you a non-transferable, non-exclusive license to use this script for the duration of the
-# subscription. If you'd like updates or support sign up at https://www.mobidelio.com or
-# email getsupport@mobidelio.com for more details
-#
-# You SHOULD NOT redistribute this source code with or without modification.
-#
-# The Mobidelio Software is provided by Mobidelio on an "AS IS" basis.  Mobidelio
-# MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF
-# NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE
-# Mobidelio SOFTWARE OR ITS USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-#
-# IN NO EVENT SHALL Mobidelio BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE,
-# REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE Mobidelio SOFTWARE, HOWEVER CAUSED AND
-# WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR
-# OTHERWISE, EVEN IF Mobidelio HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-#########################################################################################################
+## Copyright (c) Mobidelio Professional Services.
+## Licensed under the Apache 2.0 License.
 
-# Copyright Mobidelio 2025. All Rights Reserved.
-# Provide Feedback: feedback@mobidelio.com
+## This Mobidelio Software is provided by Mobidelio on an "AS IS" basis.  Mobidelio
+## MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF
+## NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE
+## Mobidelio SOFTWARE OR ITS USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+##
+## IN NO EVENT SHALL Mobidelio BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL
+## DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+## OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE,
+## REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE Mobidelio SOFTWARE, HOWEVER CAUSED AND
+## WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR
+## OTHERWISE, EVEN IF Mobidelio HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#########################################################################################################
-# Script uses powermetrics tool to capture performance data from a Mac computer.
-#########################################################################################################
+##
+## Script uses powermetrics tool to capture performance data from a Mac computer.
+##
 
-# Define variables
+set -x
+
+# Script variables
 sampleInterval="30"                                           # Interval for taking samples in seconds
 runningTime="1"                                               # Total running time in hours
 workingDirectory="/Library/Mobidelio/Powermetrics/telemetry"  # Directory where script saves powermetrics results
